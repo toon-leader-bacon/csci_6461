4310ef5e0ec2b36502fe9d753b873bc827c717ed
Arthur Bacon <bacon247@gmail.com>
2024-02-03 13:13:18 -0500

    Nocab refactored and moved some tests to a more natural spot

--- Assembler/test/DataStructures/FieldTest.java
+++ Assembler/test/DataStructures/FieldTest.java
@@ -1,23 +1,23 @@
-package org.nocab.nocabmachine.nocab;
+package DataStructures;
 
-import org.nocab.nocabmachine.nocab.DataStructures.Field;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FieldTest {
     @org.junit.jupiter.api.Test
     void toBinStringTests() {
-        Field underTest = new Field((short)1, (short)1);
+        Field underTest = new Field((short) 1, (short) 1);
         assertEquals(underTest.toBinString(), "1");
 
-        underTest = new Field((short)1, (short)6);
+        underTest = new Field((short) 1, (short) 6);
         assertEquals(underTest.toBinString(), "000001");
 
-        underTest = new Field((short)2, (short)6);
+        underTest = new Field((short) 2, (short) 6);
         assertEquals(underTest.toBinString(), "000010");
 
-        underTest = new Field((short)4, (short)6);
+        underTest = new Field((short) 4, (short) 6);
         assertEquals(underTest.toBinString(), "000100");
 
-        underTest = new Field((short)16, (short)6);
+        underTest = new Field((short) 16, (short) 6);
         assertEquals(underTest.toBinString(), "010000");
     }
 }
