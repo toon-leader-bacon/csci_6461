a64421e051a0fbcbc86ca869f71cd5d805a3ba67
Ari Majumdar <akm303@gwmail.gwu.edu>
2024-02-08 22:02:22 -0500

    started files for the two test programs

--- test/SourceProgramReader/SourceProgramReaderTest.java
+++ test/SourceProgramReader/SourceProgramReaderTest.java
@@ -53,7 +53,7 @@ class SourceProgramReaderTest {
     @org.junit.jupiter.api.Test
     void runExampleInputFile() {
         SourceProgramReader underTest = new SourceProgramReader();
-        underTest.OpenAndReadSourceProgramFile("Assembler/test/SourceProgramReader/TestProgram.txt",
+        underTest.OpenAndReadSourceProgramFile("Assembler/test/SourceProgramReader/ExampleProgram.txt",
                 true);
 
         // 21 JZ 1,0
